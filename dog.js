1931f400a10c08a2663d3be982df435c9d9f2904
Ngo Quoc Hieu <quochieutb12@gmail.com>
2020-08-05 16:47:01 +0700

    Add Method sayHi

---
+++ dog.js
@@ -0,0 +1,12 @@
+var chalk = require('chalk');
+function Dog(name){
+	this.name = name;
+	this.stomach = [];
+}
+Dog.prototype.eat = function(cat) {
+	this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function(){
+	console.log("Hi. I'm dog. My name is " + chalk.red(this.name));
+}
+module.exports = Dog;
